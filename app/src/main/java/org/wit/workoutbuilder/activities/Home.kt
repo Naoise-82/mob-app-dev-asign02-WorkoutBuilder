d5899fcc61d0a57ee41af421e7eb267502c65ecb
Esioan <esioan667@gmail.com>
2021-12-21 23:02:00 +0000

    Fragments implemented, app functionallity and navigation yet to be implemented

--- app/src/main/java/org/wit/workoutbuilder/activities/Home.kt
+++ app/src/main/java/org/wit/workoutbuilder/activities/Home.kt
@@ -3,15 +3,15 @@ package org.wit.workoutbuilder.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
 import org.wit.workoutbuilder.R
 import org.wit.workoutbuilder.databinding.HomeBinding
 
 class Home : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var homeBinding : HomeBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -19,17 +19,22 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         homeBinding = HomeBinding.inflate(layoutInflater)
-        homeBinding = HomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
+
+        drawerLayout = homeBinding.drawerLayout
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-        appBarConfiguration = AppBarConfiguration(setOf(
+       appBarConfiguration = AppBarConfiguration(setOf(
             R.id.exerciseFragment, R.id.exerciseListFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
 
     }
 
